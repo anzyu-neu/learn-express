1d4130a1484d1a02efe7741bf73f1f0af780ee3e
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-03-12 16:07:43 -0400

    modularized the code

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,27 @@
-const express = require('express')
+const express = require('express');
+const { model } = require('mongoose');
 const router = express.Router();
+
+router.get('/usernames', (reg, res) => {
+    let usernames = req.users.map(function(user) {
+        return {id: user.id, username: user.username}
+    });
+    res.send(usernames);
+});
+
+router.get('/username/:name', (req, res) => {
+    let name = req.params.name;
+    let users_with_name = req.users.filter(function(user) {
+      return user.username === name;
+    });
+    console.log(users_with_name);
+    if (users_with_name.length === 0) {
+      res.send({
+        error: {message: `${name} not found`, status: 404}
+      });
+    } else {
+      res.send(users_with_name);
+    }
+});
+
+module.exports = router
